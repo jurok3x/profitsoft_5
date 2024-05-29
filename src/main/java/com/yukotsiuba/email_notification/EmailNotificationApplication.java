bc18508c3f073212e1cfa1575ad7c0013009d7c8
yurii kotsiuba <jurok3x@gmail.com>
2024-05-29 19:45:54 +0300

    First commit.

---
+++ src/main/java/com/yukotsiuba/email_notification/EmailNotificationApplication.java
@@ -0,0 +1,13 @@
+package com.yukotsiuba.email_notification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailNotificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailNotificationApplication.class, args);
+	}
+
+}
