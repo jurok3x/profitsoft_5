dd5255093e2d40355bd1087158d547bfdc0a83a2
ykotsiuba <jurok3x@gmail.com>
2024-06-07 13:45:57 +0300

    Updated email sender service. .

--- src/main/java/com/yukotsiuba/email_notification/service/MailSender.java
+++ src/main/java/com/yukotsiuba/email_notification/service/MailSender.java
@@ -4,6 +4,4 @@ import com.yukotsiuba.email_notification.dto.EmailMessageDto;
 
 public interface MailSender {
     void sendEmail(EmailMessageDto emailMessageDto);
-
-    void sendFailedEmails();
 }
--- src/main/java/com/yukotsiuba/email_notification/service/impl/MailSenderImpl.java
+++ src/main/java/com/yukotsiuba/email_notification/service/impl/MailSenderImpl.java
@@ -3,7 +3,8 @@ package com.yukotsiuba.email_notification.service.impl;
 import com.yukotsiuba.email_notification.dto.EmailMessageDto;
 import com.yukotsiuba.email_notification.entity.EmailMessage;
 import com.yukotsiuba.email_notification.entity.EmailStatus;
-import com.yukotsiuba.email_notification.repository.EmailMessageRepository;
+import com.yukotsiuba.email_notification.mapper.EmailMessageMapper;
+import com.yukotsiuba.email_notification.service.EmailMessageService;
 import com.yukotsiuba.email_notification.service.MailSender;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -23,62 +24,12 @@ public class MailSenderImpl implements MailSender {
 
     private final JavaMailSender mailSender;
 
-    private final EmailMessageRepository emailMessageRepository;
+    private final EmailMessageService emailMessageService;
 
     @Override
     public void sendEmail(EmailMessageDto emailMessageDto) {
         log.info("Sending email message {}", emailMessageDto);
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(emailMessageDto.getTo());
-        message.setSubject(emailMessageDto.getSubject());
-        message.setText(emailMessageDto.getText());
-        message.setFrom("noreply@example.com");
-
-        try {
-            mailSender.send(message);
-            EmailMessage emailMessage = EmailMessage.builder()
-                    .to(emailMessageDto.getTo())
-                    .content(emailMessageDto.getText())
-                    .subject(emailMessageDto.getSubject())
-                    .attempts(1)
-                    .status(EmailStatus.SENT)
-                    .lastSentTime(LocalDateTime.now().atZone(ZoneId.systemDefault()).toInstant())
-                    .build();
-            emailMessageRepository.save(emailMessage);
-        } catch (Exception ex) {
-            log.error("Could not send email: {}", ex.getMessage());
-            EmailMessage emailMessage = EmailMessage.builder()
-                    .to(emailMessageDto.getTo())
-                    .content(emailMessageDto.getText())
-                    .subject(emailMessageDto.getSubject())
-                    .attempts(1)
-                    .status(EmailStatus.ERROR)
-                    .lastSentTime(LocalDateTime.now().atZone(ZoneId.systemDefault()).toInstant())
-                    .build();
-            emailMessageRepository.save(emailMessage);
-        }
-    }
-
-    @Override
-    public void sendFailedEmails() {
-        List<EmailMessage> failedEmails = emailMessageRepository.findByStatus(EmailStatus.ERROR);
-
-        for (EmailMessage email : failedEmails) {
-            try {
-                sendEmail(EmailMessageDto.builder()
-                        .subject(email.getSubject())
-                        .to(email.getTo())
-                        .text(email.getContent())
-                        .build());
-                email.setStatus(EmailStatus.SENT);
-                email.setErrorMessage(null);
-                email.setAttempts(email.getAttempts() + 1);
-            } catch (MailException e) {
-                email.setErrorMessage(e.getMessage());
-                email.setAttempts(email.getAttempts() + 1);
-                email.setLastSentTime(LocalDateTime.now().atZone(ZoneId.systemDefault()).toInstant());
-            }
-            emailMessageRepository.save(email);
-        }
+        SimpleMailMessage message = EmailMessageMapper.toSimpleMailMessage(emailMessageDto);
+        mailSender.send(message);
     }
 }
